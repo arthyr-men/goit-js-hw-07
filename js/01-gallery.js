a93f2352d43f4478d00f2adf07bf53798c4a55a3
Artur-man <123097639+arthyr-men@users.noreply.github.com>
2023-07-22 11:42:55 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,21 +5,20 @@ console.log(galleryItems);
 
 const containerGalleryItems = document.querySelector('.gallery');
 
-const createGalleryMarkyp = galleryItems
-  .map(
-    ({ preview, original, description }) => `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`,
-  )
+const createGalleryMarkup = galleryItems
+  .map(({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`)
   .join('');
-containerGalleryItems.insertAdjacentHTML('beforeend', createGalleryMarkyp);
+
+containerGalleryItems.insertAdjacentHTML('beforeend', createGalleryMarkup);
 
 containerGalleryItems.addEventListener('click', onImgClick);
 
@@ -33,17 +32,22 @@ function onhandleEscapeKeydown(instance) {
 
 function onImgClick(event) {
   event.preventDefault();
-  const instance = basicLightbox.create(
-    ` <img src="${event.target.dataset.source}" width="800" height="600">`,
-    {
-      onShow: instance => {
-        document.addEventListener('keydown', handleEscapeKeydown);
+  const clickedElement = event.target;
+
+  // Check if the clicked element has the class 'gallery__image'
+  if (clickedElement.classList.contains('gallery__image')) {
+    const instance = basicLightbox.create(
+      ` <img src="${clickedElement.dataset.source}" width="800" height="600">`,
+      {
+        onShow: (instance) => {
+          document.addEventListener('keydown', handleEscapeKeydown);
+        },
+        onClose: (instance) => {
+          document.removeEventListener('keydown', handleEscapeKeydown);
+        },
       },
-      onClose: instance => {
-        document.removeEventListener('keydown', handleEscapeKeydown);
-      },
-    },
-  );
-  const handleEscapeKeydown = onhandleEscapeKeydown(instance);
-  instance.show();
+    );
+    const handleEscapeKeydown = onhandleEscapeKeydown(instance);
+    instance.show();
+  }
 }
